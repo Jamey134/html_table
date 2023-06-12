3465681ea37a6de5d6e6b308d81b262d5a9bdc62
James Watson <jameywatson134@gmail.com>
2023-06-11 18:27:28 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask  
+app = Flask(__name__)   
+
+@app.route('/')      
+def render_lists():
+
+
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+
+
+app.run(debug=True, port=5001)    # Run the app in debug mode.
+
